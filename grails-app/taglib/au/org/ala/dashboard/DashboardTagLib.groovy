ebea5c3c0a038958b796a3206013b2125905527a
markew <mark.woolston@axon.com.au>
2012-05-17 03:43:16 +0000

    tag to shorten long strings
    git-svn-id: http://ala-dashboard.googlecode.com/svn/trunk/dashboard@22 abfdd4b4-e21d-91d3-f252-ce0d7bae99a4

--- grails-app/taglib/au/org/ala/dashboard/DashboardTagLib.groovy
+++ grails-app/taglib/au/org/ala/dashboard/DashboardTagLib.groovy
@@ -4,10 +4,51 @@ class DashboardTagLib {
 
     static namespace = 'db'
     def metadataService
-    
+
+    /**
+     * Formats numbers for humans.
+     *
+     * @attr value to format
+     */
     def formatNumber = { attrs ->
         if (attrs.value) {
             out << metadataService.format(attrs.value as int)
         }
     }
+
+    /**
+     * Shortens a string to the specified maximum length.
+     *
+     * @attr text to shorten
+     * @attr size max length
+     */
+    def shorten = { attrs ->
+        def len = attrs.size.toInteger()
+        if (len && attrs.text.size() > len) {
+            out << trimLength(attrs.text, len)
+        }
+        else {
+            out << attrs.text
+        }
+    }
+
+    /**
+     * Trims the passed string to the specified length breaking at word boundaries and adding an ellipsis if trimmed.
+     */
+    def trimLength = {trimString, stringLength ->
+
+        String concatenateString = "..."
+        List separators = [".", " "]
+
+        if (stringLength && (trimString?.length() > stringLength)) {
+            trimString = trimString.substring(0, stringLength - concatenateString.length())
+            String separator = separators.findAll{trimString.contains(it)}?.min{trimString.lastIndexOf(it)}
+            if(separator){
+                trimString = trimString.substring(0, trimString.lastIndexOf(separator))
+            }
+            trimString += concatenateString
+        }
+        return trimString
+    }
+
 }
