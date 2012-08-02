bbde3b33151e41ed92b3769c53cf8b6b6a25b438
markew <mark.woolston@axon.com.au>
2012-08-02 03:48:46 +0000

    add external config management
    git-svn-id: http://ala-dashboard.googlecode.com/svn/trunk/dashboard@48 abfdd4b4-e21d-91d3-f252-ce0d7bae99a4

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,14 +1,27 @@
-// locations to search for config files that get merged into the main config
-// config files can either be Java properties files or ConfigSlurper scripts
-
-// grails.config.locations = [ "classpath:${appName}-config.properties",
-//                             "classpath:${appName}-config.groovy",
-//                             "file:${userHome}/.grails/${appName}-config.properties",
-//                             "file:${userHome}/.grails/${appName}-config.groovy"]
-
-// if (System.properties["${appName}.config.location"]) {
-//    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
-// }
+/******************************************************************************\
+ *  CONFIG MANAGEMENT
+ \******************************************************************************/
+def ENV_NAME = "DASHBOARD_CONFIG"
+def default_config = "/data/collectory/config/${appName}-config.properties"
+if(!grails.config.locations || !(grails.config.locations instanceof List)) {
+    grails.config.locations = []
+}
+if(System.getenv(ENV_NAME) && new File(System.getenv(ENV_NAME)).exists()) {
+    println "Including configuration file specified in environment: " + System.getenv(ENV_NAME);
+    grails.config.locations = ["file:" + System.getenv(ENV_NAME)]
+} else if(System.getProperty(ENV_NAME) && new File(System.getProperty(ENV_NAME)).exists()) {
+    println "Including configuration file specified on command line: " + System.getProperty(ENV_NAME);
+    grails.config.locations = ["file:" + System.getProperty(ENV_NAME)]
+} else if(new File(default_config).exists()) {
+    println "Including default configuration file: " + default_config;
+    def loc = ["file:" + default_config]
+    println ">> loc = " + loc
+    grails.config.locations = loc
+    println "grails.config.locations = " + grails.config.locations
+} else {
+    println "No external configuration file defined."
+}
+println "(*) grails.config.locations = ${grails.config.locations}"
 
 /******************************************************************************\
  *  EXTERNAL SERVERS
