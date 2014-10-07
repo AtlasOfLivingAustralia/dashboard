6b7e8ae5724e77277f202eb4defe69258947de05
mbohun <martin.bohun@gmail.com>
2014-10-07 21:18:36 +1100

    grails.project.groupId set to au.org.ala in order to deploy the war file into the correct dir/package structure

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -4,7 +4,8 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.groupId = "au.org.ala"
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
