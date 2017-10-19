e5f7b65ec7f3dc0dff275932272c9d3efdd1f58e
Patricia Koh <patricia.koh@csiro.au>
2017-10-19 15:32:42 +1100

    Update volunteer portal stats url

--- src/main/groovy/au/org/ala/dashboard/Constants.groovy
+++ src/main/groovy/au/org/ala/dashboard/Constants.groovy
@@ -17,7 +17,7 @@ class Constants {
 
         final static String PARTIAL_URL_DATE_STATS_LATEST_RECORD_WITH_IMAGE = "/ws/occurrences/search?q=!assertions:invalidCollectionDate%20AND%20occurrence_date:%5B*%20TO%20*%5D&pageSize=1&sort=first_loaded_date&dir=desc&facet=off&fq=multimedia:Image"
 
-        final static String PARTIAL_URL_VOLUNTEER_STATS = "/ws/stats"
+        final static String PARTIAL_URL_VOLUNTEER_STATS = "/ws/stats.json"
 
         final static String PARTIAL_URL_SPECIES_BY_DECADE = "/ws/explore/groups.json?q=*:*&pageSize=10&fq=occurrence_year:"
 
