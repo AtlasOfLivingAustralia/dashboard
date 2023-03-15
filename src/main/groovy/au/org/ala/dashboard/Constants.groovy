03439e0ff78d601942fe81e3aad7f594becc9404
Sushant <105408469+sughics@users.noreply.github.com>
2023-03-15 13:03:44 +1100

    issue #119 adding usage of ws/repositoryStatistics for images stats lookup for api gateway usage

--- src/main/groovy/au/org/ala/dashboard/Constants.groovy
+++ src/main/groovy/au/org/ala/dashboard/Constants.groovy
@@ -1,11 +1,15 @@
 package au.org.ala.dashboard
 
+import grails.core.GrailsApplication
+
 /**
  * Created by rui008 on 26/11/2014.
  */
 class Constants {
 
     class WebServices {
+
+        def grailsApplication
 
         final static String PARTIAL_URL_STATE_TERRITORY_FACETED_RESULTS = "/occurrences/search.json?pageSize=0&q=*:*&facets=state&flimit=200"
 
@@ -17,7 +21,7 @@ class Constants {
 
         final static String PARTIAL_URL_DATE_STATS_LATEST_RECORD_WITH_IMAGE = "/occurrences/search?q=!assertions:invalidCollectionDate%20AND%20occurrence_date:%5B*%20TO%20*%5D&pageSize=1&sort=first_loaded_date&dir=desc&facet=off&fq=multimedia:Image"
 
-        final static String PARTIAL_URL_DATASETS_CONTAIN_DATA = "/occurrence/facets?q=*:*&facets=data_resource_uid&flimit=0"
+        final static String PARTIAL_URL_DATASETS_CONTAIN_DATA = "/occurrences/facets?q=*:*&facets=data_resource_uid&flimit=0"
 
         final static String PARTIAL_URL_VOLUNTEER_STATS = "/ws/stats.json"
 
@@ -41,21 +45,21 @@ class Constants {
 
         final static String PARTIAL_URL_LOGGER_EMAIL_BREAKDOWN = "/service/emailBreakdown?eventId=1002"
 
-        final static String PARTIAL_URL_TAXA_WITH_IMAGES = "/occurrence/facets?facets=taxon_name&pageSize=0&q=multimedia:Image"
+        final static String PARTIAL_URL_TAXA_WITH_IMAGES = "/occurrences/facets?facets=taxon_name&pageSize=0&q=multimedia:Image"
 
-        final static String PARTIAL_URL_SPECIES_WITH_IMAGES = "/occurrence/facets?q=multimedia:Image%20AND%20(rank:species%20OR%20rank:subspecies)&facets=taxon_name&pageSize=0"
+        final static String PARTIAL_URL_SPECIES_WITH_IMAGES = "/occurrences/facets?q=multimedia:Image%20AND%20(rank:species%20OR%20rank:subspecies)&facets=taxon_name&pageSize=0"
 
-        final static String PARTIAL_URL_SUBSPECIES_WITH_IMAGES = "/occurrence/facets?q=multimedia:Image%20AND%20rank:subspecies&facets=taxon_name&pageSize=0"
+        final static String PARTIAL_URL_SUBSPECIES_WITH_IMAGES = "/occurrences/facets?q=multimedia:Image%20AND%20rank:subspecies&facets=taxon_name&pageSize=0"
 
-        final static String PARTIAL_URL_TAXA_VP_COUNT = "/occurrence/facets?facets=taxon_name&pageSize=0&q=multimedia:Image"
+        final static String PARTIAL_URL_TAXA_VP_COUNT = "/occurrences/facets?facets=taxon_name&pageSize=0&q=multimedia:Image"
 
-        final static String PARTIAL_URL_TAXA_CS_COUNT = "/occurrence/facets?facets=taxon_name&pageSize=0&q=multimedia:Image%20AND%20provenance:%22Individual%20sightings%22"
+        final static String PARTIAL_URL_TAXA_CS_COUNT = "/occurrences/facets?facets=taxon_name&pageSize=0&q=multimedia:Image%20AND%20provenance:%22Individual%20sightings%22"
 
         final static String PARTIAL_URL_IMAGE_TOTAL = "/ws/getRepositoryStatistics"
+        final static String API_GW_PARTIAL_URL_IMAGE_TOTAL = "/ws/repositoryStatistics"
 
         final static String PARTIAL_URL_COUNT_RECORDS = "/occurrences/search?q=*:*&pageSize=0&facet=off"
 
-        final
         static String PARTIAL_URL_ACCEPTED_NAMES = "/search?q=(taxonomicStatus:accepted%20OR%20taxonomicStatus:inferredAccepted)&fq=idxtype:TAXON"
 
         final
